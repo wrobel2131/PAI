627bf0a8c275ef531907c44986f3b515ea94a050
Dawid Wrobel <wrobelek2131@wp.pl>
2022-02-04 10:20:35 +0100

    added .gitignore file, database, docker configuration changes, routing and index.php

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
 <?php
 
-echo "Hello there 😅";
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url( $path, PHP_URL_PATH);
+
+// print $path;
+Routing::get('', 'DefaultController');
+Routing::get('home', 'SecurityController');
+Routing::get('programs', 'ProgramController');
+Routing::get('exercises', 'ExerciseController');
+Routing::get('calculator', 'DefaultController');
+Routing::get('settings', 'DefaultController');
+Routing::get('signIn', 'DefaultController');
+Routing::get('signUp', 'DefaultController');
+Routing::get('previousLogs', 'DefaultController');
+Routing::get('specificWorkout', 'DefaultController');
+Routing::get('specificProgram', 'WorkoutController');
+Routing::get('exerciseInWorkout', 'DefaultController');
+Routing::get('addWorkout', 'DefaultController');
+Routing::get('addProgram', 'ProgramController');
+Routing::get('addExerciseToWorkout', 'DefaultController');
+Routing::get('addExercise', 'DefaultController');
+
+Routing::post('login', 'SecurityController');
+Routing::post('register', 'SecurityController');
+Routing::post('logout', 'SecurityController');
+
+//Routing::post('settings', 'DefaultController');
+Routing::post('addProgram', 'ProgramController');
+
+Routing::post('addWorkoutToProgram', 'WorkoutController');
+
+
+
+Routing::run($path);
